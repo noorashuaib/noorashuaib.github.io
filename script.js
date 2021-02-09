221576f7e4767ca7f2a857132600d831ba714c08
Noora Shuaib <ns4144@nyu.edu>
2021-02-10 02:28:26 +0400

    Update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // JAVASCRIPT
 
-//                    *See citations in indexNew.html*
+//                    *See citations in index.html*
 
 // This script serves the dropdown menu. The function is triggered when the
 // mouse clicks on the menu button, showing the full menu.
@@ -11,7 +11,7 @@ function myFunction() {
 
 // When the menu is down, a function is applied to reach the content of the
 // button selected. The number of pages affects the dropdown length as seen with
-// var and the use of i < dropdowns.length; i++. 
+// var and the use of i < dropdowns.length; i++.
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
